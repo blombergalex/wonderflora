deda0bd04ef9a475de55992c8b82df471a0bbe88
Alexandra Blomberg <blombergalexandras@gmail.com>
2024-02-27 16:05:37 +0100

    add content for seeds page

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-import express from "express";
-import flowRouter from "./routes/flowers.js";
-import plantRouter from "./routes/plants.js";
-import seedRouter from "./routes/seeds.js";
+import express from 'express';
+import flowRouter from './routes/flowers.js';
+import plantRouter from './routes/plants.js';
+import seedRouter from './routes/seeds.js';
 import * as path from 'path';
 
 const app = express();
 const port = process.env.PORT || 3003;
 const __dirname = path.resolve();
 
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
     res.render(
-        "pages/home",
+        'pages/home',
         {
-            pageTitle: "Wonderflora",
-            subTitle: "Welcome to this wonderful floral shop",
+            pageTitle: 'Wonderflora',
+            subTitle: 'Welcome to this wonderful floral shop',
         }
     )
 })
@@ -27,6 +27,6 @@ app.use('/plants', plantRouter)
 
 app.use('/seeds', seedRouter)
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 app.listen(port, () => console.log(`Listening on port: ${port}`));
